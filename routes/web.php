b302728360bbee3e2b47c843b7ce7d67701c1507
Erik Figueiredo <erik.figueiredo@gmail.com>
2017-06-05 09:10:57 -0300

    rotas para o webhook do bot

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,10 @@ Route::get('/', function () {
     dd($text);
     return view('welcome');
 });
+
+
+Route::prefix('bot')
+    ->group(function() {
+        Route::get('/webhook', 'BotController@subscribe');
+        Route::post('/webhook', 'BotController@receiveMessage');
+    });
