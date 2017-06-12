e042fb845b224bd4ad5b8d0a676707e68609c398
Erik Figueiredo <erik.figueiredo@gmail.com>
2017-06-12 09:11:59 -0300

    checagem dos dados nos ervidor de homologaçao

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -16,6 +16,7 @@ use CodeBot\TemplatesMessage\ListTemplate;
 use CodeBot\WebHook;
 use CodeBot\Message\Text;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class BotController extends Controller
 {
@@ -64,15 +65,18 @@ class BotController extends Controller
         $button = new Button('web_url', null, 'http://www.php.net/');
         $product2 = new Product('Produto 2', 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/1200px-PHP-logo.svg.png', 'Curso de PHP', $button);
 
-        $template = new GenericTemplate($senderId);
+        /*$template = new GenericTemplate($senderId);
         $template->add($product);
         $template->add($product2);
-        $callSendApi->make($template->message('qwe'));
+        $callSendApi->make($template->message('qwe'));*/
 
         $button = new Button('web_url', null, 'https://angular.io/');
+        Log::info(json_encode($button->get()));
         $product = new Product('Produto 1', 'https://media.licdn.com/mpr/mpr/AAEAAQAAAAAAAAqfAAAAJDQwZWJiNTdkLThiYjUtNGQ2YS1iMzJjLTRiMmQ5YjZiMDNiNw.png', 'Curso de angular', $button);
 
         $button = new Button('web_url', null, 'http://www.php.net/');
+        Log::info(json_encode($button->get()));
+        return '';
         $product2 = new Product('Produto 2', 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/1200px-PHP-logo.svg.png', 'Curso de PHP', $button);
 
         $template = new ListTemplate($senderId);
